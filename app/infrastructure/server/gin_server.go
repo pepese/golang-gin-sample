1d9f95d416a44daab2cb8275d3ea165af173a65f
pepese <tanakakns@LABO-MAC-022.local>
2020-03-05 16:04:43 +0900

    update

--- app/infrastructure/server/gin_server.go
+++ app/infrastructure/server/gin_server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -27,6 +29,16 @@ func NewGinServer() *gin.Engine {
 	return e
 }
 
+type responseBodyWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w responseBodyWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
 func accessLogging() gin.HandlerFunc {
 	// Logging Skip Logic
 	notlogged := []string{"/healthcheck"}
@@ -38,6 +50,9 @@ func accessLogging() gin.HandlerFunc {
 		}
 	}
 	return func(c *gin.Context) {
+		// context.Context
+		ctx := context.Background()
+
 		// Request ID
 		reqId := c.Request.Header.Get("X-Request-Id")
 		if reqId == "" {
@@ -48,9 +63,6 @@ func accessLogging() gin.HandlerFunc {
 		}
 		c.Set("reqId", reqId)
 		c.Writer.Header().Set("X-Request-Id", reqId)
-
-		// context.Context
-		ctx := context.Background()
 
 		// Logger
 		logger := app.GetLoggerWithKeyValue("reqId", reqId)
@@ -66,13 +78,31 @@ func accessLogging() gin.HandlerFunc {
 			path = path + "?" + query
 		}
 		header := c.Request.Header
+		// リクエスト取得
+		req := c.Request
+		requestBody := make([]byte, 0)
+		if req.Body != nil {
+			body, err := ioutil.ReadAll(req.Body)
+			req.Body.Close()
+			if err != nil {
+				logger.Errorw("read request body is fail:", err)
+			}
+			req.Body = ioutil.NopCloser(bytes.NewReader(body)) // streamを読み尽くすので入れ直す
+			requestBody = body
+		}
+		// レスポンス取得
+		rbw := &responseBodyWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+		c.Writer = rbw
+		// アクセスログ（リクエスト）出力
+		logger.Infow("===>request", "reqId", reqId, "method", method, "path", path, "header", fmt.Sprintf("%#v", header), "request", string(requestBody))
 
 		c.Next()
 
 		if _, ok := skip[path]; !ok {
 			responseTime := time.Now().Sub(start)
 			statusCode := c.Writer.Status()
-			logger.Infow("access log", "method", method, "path", path, "header", fmt.Sprintf("%#v", header), "responseTime", responseTime, "statusCode", statusCode)
+			// アクセスログ（レスポンス）出力
+			logger.Infow("<==response", "reqId", reqId, "statusCode", statusCode, "responseTime", responseTime, "response", rbw.body.String())
 		}
 	}
 }
